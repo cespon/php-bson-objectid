ce34a8c86d4558f66374b6add5ffd5d162d8331c
Julián Cespón <julian.cespon@gmail.com>
2017-01-25 16:00:24 +0100

    static

--- objectid.php
+++ objectid.php
@@ -4,30 +4,30 @@ class ObjectID
 {
     private static $MACHINE_ID, $index, $pid;
 
-    private function init()
+    private static function init()
     {
         self::$MACHINE_ID = intval(self::rand() * 0xFFFFFF);
         self::$index = intval(self::rand() * 0xFFFFFF);
         self::$pid = getmypid() % 0xFFFF;
     }
 
-    private function rand()
+    private static function rand()
     {
         return mt_rand(0, mt_getrandmax() - 1) / mt_getrandmax();
     }
 
-    private function hex($length, $n)
+    private static function hex($length, $n)
     {
         $n = dechex($n);
         return (strlen($n) == $length)? $n : str_repeat("0", $length-strlen($n)) . $n;
     }
 
-    private function next()
+    private static function next()
     {
         return self::$index = (self::$index + 1) % 0xFFFFFF;
     }
 
-    public function generate()
+    public static function generate()
     {
         self::init();
 
