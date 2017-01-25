97bf3d77a629e632b621bab3d4cef62e4a279780
Julián Cespón <julian.cespon@gmail.com>
2017-01-25 13:55:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# PHP BSON ObjectID
+
+This PHP class allows you to create ObjectIDs without dependencies or the MongoDB driver.
+
+Based in [bson-objectid](https://github.com/williamkapke/bson-objectid).
+
+
+##Usage
+
+    $id = ObjectID::generate(); //5888a045e4b30039941b35b0
+
+##License
+
+Apache v2.0
---
+++ objectid.php
@@ -0,0 +1,44 @@
+<?php
+
+class ObjectID
+{
+	private static $MACHINE_ID, $index, $pid;
+
+	private function init()
+	{
+		self::$MACHINE_ID = intval(self::rand() * 0xFFFFFF);
+		self::$index = intval(self::rand() * 0xFFFFFF);
+		self::$pid = getmypid() % 0xFFFF;
+	}
+
+	private function rand()
+	{
+		return mt_rand(0, mt_getrandmax() - 1) / mt_getrandmax();
+	}
+
+	private function hex($length, $n)
+	{
+		$n = dechex($n);
+		return (strlen($n) == $length)? $n : str_repeat("0", $length-strlen($n)) . $n;
+	}
+
+	private function next($index)
+	{
+		return self::$index = ($index + 1) % 0xFFFFFF;
+	}
+
+	public function generate()
+	{
+		self::init();
+
+		$time = round(microtime(true));
+
+		return self::hex(8, $time) . 
+			   self::hex(6, self::$MACHINE_ID) .
+			   self::hex(4, self::$pid) . 
+			   self::hex(6, self::next( self::$index ));
+	}
+}
+
+$id = ObjectID::generate();
+echo $id;
